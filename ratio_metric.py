fa5818b25a85c9413b6bb67e4402e1c58e1602ae
GraniLuk <granica.lukasz@gmail.com>
2025-01-23 06:39:56 +0100

    Remove not used metrics

--- ratio_metric.py
+++ ratio_metric.py
@@ -1,15 +1,3 @@
-from opencensus.ext.azure.common import AzureMonitorContext
-from opencensus.ext.azure.trace_exporter import AzureExporter
-from opencensus.ext.azure.log_exporter import AzureLogHandler
-from opencensus.ext.azure.metrics_exporter import new_metrics_exporter
-from opencensus.metrics.transport import get_exporter_thread
-from opencensus.metrics import label_key, label_value, measure, metric, view, aggregation
-
-from opencensus.stats import stats as stats_module
-from opencensus.tags import tag_map as tag_map_module
-
-from opencensus.ext.azure.metrics_exporter import MetricsExporter
-
 # Use TelemetryClient to send custom metrics
 from opencensus.ext.azure import telemetry_client
 
